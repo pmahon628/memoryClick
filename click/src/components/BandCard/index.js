1361dd77a10ccc3b4f9b09a3169ee24cab1c6696
Patrick Mahon <patrickmahon@Patricks-MacBook-Pro.local>
2019-08-04 21:20:22 -0400

    styles etc

--- click/src/components/BandCard/index.js
+++ click/src/components/BandCard/index.js
@@ -5,17 +5,23 @@ function BandCard(props){
     return(
      <div className="Card">
        <div className="img-container">
+         <div className="row">
+         <div className="col">
          <img alt={props.name} src={props.image} />
           </div>
           <div className="content">
+          </div>
+          <div className="col">
            <ul>
                <li>
                    <strong>Name:</strong>{props.name}
-                </li>
+                   </li>
                 <li>
                     <strong>Genre:</strong>{props.genre}
                 </li>
            </ul>
+           </div>
+          </div>
           </div>
             <span onClick={() => props.removeFriends(props.id)} className="remove">x
             </span>
