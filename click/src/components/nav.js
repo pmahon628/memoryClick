36f63abb7149fb42d60a9739a0c66133ea0ae7f5
Patrick Mahon <p.mahon0628@gmail.com>
2019-10-28 21:43:27 -0400

    routes

--- click/src/components/nav.js
+++ click/src/components/nav.js
@@ -1,84 +1,25 @@
 import React, {Component} from "react";
 import { BrowserRouter as Router, Redirect } from "react-router-dom";
 
-import Axios from "axios";
+// import Axios from "axios";
 
 class pageNav extends Component {
 
-  constructor(){
-    super()
-    this.state ={
-      redirectTo: null,
-      loggedIn: false,
-      name: null
-    }
-    this.logout =this.logout.bind(this);
-  }
-
   return (
   <Router>
-    <nav className="navbar navbar-expand-lg navbar-light bg-light" id="nav">
-      {loggedIn ? (
-        <a className="navbar-brand" href="/">{name}'s College App</a>
-      ):(
-        <a className="navbar-brand" href="/">My College App</a>
-      )}
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-      <div className="collapse navbar-collapse" id="navbarText">
-      <ul className="navbar-nav mr-auto">
-        <li className="nav-item active">
-          
-          <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
-         
+    <ul>
         </li>
         <li className="nav-item">
           
-          <a className="nav-link" href="/search">Search</a>
+          <a className="nav-link" href="/bands">Band Memory</a>
           
         </li>
         <li className="nav-item">
          
-          <a className="nav-link" href="/saved">Saved</a>
-         
-        </li>
-        <li className="nav-item">
-          
-          <a className="nav-link" href="/faq">FAQ</a>
-          
-        </li>
-      </ul>
+          <a className="nav-link" href="/movies">Movies Memory</a>
+        </ul>
         
-      {loggedIn ? (
-          <span></span>
-      ) : (
-        <span className="navbar-text">
-          <a className="nav-link" href="/register">Sign up  </a>
-        </span>
-      )
-      }
-    
-        {loggedIn ? (
-          <span className="nvabar-text">
-          <a className="nav-link" onClick={this.logout} href="/">Logout</a>
-          </span>
-        ):(
-  
-      
-        <span className="nvabar-text">
-         <a className= "nav-link" href="/login">Login</a>
-        </span>
-      
-        )}
-        
-      </div>
-    </nav>
     </Router>
-    
-   
- );
-  
-}
-}
+  );
+
 export default pageNav;
